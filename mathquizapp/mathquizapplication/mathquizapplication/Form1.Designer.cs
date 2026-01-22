746d2bae06f9af07f3a8f31942c64095b0a67430
marriane <marrianecebu12@gmail.com>
2026-01-22 12:18:04 +0800

    cn

---
+++ mathquizapp/mathquizapplication/mathquizapplication/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace mathquizapplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblNumA = new System.Windows.Forms.Label();
+            this.lblSymbol = new System.Windows.Forms.Label();
+            this.lblNumB = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(38, 41);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(78, 23);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblNumA
+            // 
+            this.lblNumA.AutoSize = true;
+            this.lblNumA.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumA.Location = new System.Drawing.Point(107, 101);
+            this.lblNumA.Name = "lblNumA";
+            this.lblNumA.Size = new System.Drawing.Size(27, 25);
+            this.lblNumA.TabIndex = 1;
+            this.lblNumA.Text = "0";
+            
+            // 
+            // lblSymbol
+            // 
+            this.lblSymbol.AutoSize = true;
+            this.lblSymbol.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSymbol.Location = new System.Drawing.Point(233, 101);
+            this.lblSymbol.Name = "lblSymbol";
+            this.lblSymbol.Size = new System.Drawing.Size(20, 25);
+            this.lblSymbol.TabIndex = 2;
+            this.lblSymbol.Text = ".";
+            // 
+            // lblNumB
+            // 
+            this.lblNumB.AutoSize = true;
+            this.lblNumB.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumB.Location = new System.Drawing.Point(348, 101);
+            this.lblNumB.Name = "lblNumB";
+            this.lblNumB.Size = new System.Drawing.Size(27, 25);
+            this.lblNumB.TabIndex = 3;
+            this.lblNumB.Text = "0";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(300, 172);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(86, 25);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "Result";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.Location = new System.Drawing.Point(92, 169);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(149, 33);
+            this.txtAnswer.TabIndex = 5;
+            this.txtAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.txtAnswer_TextChanged);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(130, 240);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(224, 37);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(495, 353);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblNumB);
+            this.Controls.Add(this.lblSymbol);
+            this.Controls.Add(this.lblNumA);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblNumA;
+        private System.Windows.Forms.Label lblSymbol;
+        private System.Windows.Forms.Label lblNumB;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ mathquizapp/mathquizapplication/mathquizapplication/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace mathquizapplication
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int total;      
+        int score = 0;   
+        int numA, numB;  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblScore.Text = "Score: " + score.ToString();
+            GenerateQuestion();
+        }
+
+        private void GenerateQuestion()
+        {
+            numA = rnd.Next(10, 20);
+            numB = rnd.Next(1, 9); 
+
+            int setOperator = rnd.Next(1, 5); 
+
+            if (setOperator == 1)
+            {
+                lblSymbol.Text = "+";
+                total = numA + numB;
+            }
+            else if (setOperator == 2)
+            {
+                lblSymbol.Text = "-";
+                total = numA - numB;
+            }
+            else if (setOperator == 3)
+            {
+                lblSymbol.Text = "*";
+                total = numA * numB;
+            }
+            else if (setOperator == 4)
+            {
+                lblSymbol.Text = "/";
+                total = numA / numB;   
+                numA = total * numB;  
+            }
+
+            lblNumA.Text = numA.ToString();
+            lblNumB.Text = numB.ToString();
+            txtAnswer.Text = "";
+        }
+
+        private void txtAnswer_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtAnswer.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                txtAnswer.Text = txtAnswer.Text.Remove(txtAnswer.Text.Length - 1);
+                txtAnswer.SelectionStart = txtAnswer.Text.Length;
+            }
+        }
+
+        private async void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAnswer.Text))
+            {
+                MessageBox.Show("Please enter an answer!");
+                return;
+            }
+
+            int userAnswer = Convert.ToInt32(txtAnswer.Text);
+
+            if (userAnswer == total)
+            {
+                lblAnswer.Text = "Correct";
+                lblAnswer.ForeColor = Color.Blue;
+                score++;
+                lblScore.Text = "Score: " + score.ToString();
+            }
+            else
+            {
+                lblAnswer.Text = "Incorrect";
+                lblAnswer.ForeColor = Color.Red;
+            }
+
+            await System.Threading.Tasks.Task.Delay(1000);
+
+            GenerateQuestion();
+            lblAnswer.Text = "";
+        }
+    }
+}
