301edaa1aad0a8ffa117d6291d243b8cd7fc0faf
marriane <marrianecebu12@gmail.com>
2026-01-22 09:42:04 +0800

    comm

---
+++ pong/pong/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlPlayer = new System.Windows.Forms.Panel();
+            this.pnlCPU = new System.Windows.Forms.Panel();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlPlayer
+            // 
+            this.pnlPlayer.BackColor = System.Drawing.Color.Cyan;
+            this.pnlPlayer.Location = new System.Drawing.Point(0, 265);
+            this.pnlPlayer.Name = "pnlPlayer";
+            this.pnlPlayer.Size = new System.Drawing.Size(32, 100);
+            this.pnlPlayer.TabIndex = 0;
+            // 
+            // pnlCPU
+            // 
+            this.pnlCPU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.pnlCPU.Location = new System.Drawing.Point(571, 265);
+            this.pnlCPU.Name = "pnlCPU";
+            this.pnlCPU.Size = new System.Drawing.Size(32, 100);
+            this.pnlCPU.TabIndex = 1;
+            // 
+            // picBall
+            // 
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(285, 183);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(34, 30);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 2;
+            this.picBall.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.pnlCPU);
+            this.Controls.Add(this.pnlPlayer);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPlayer;
+        private System.Windows.Forms.Panel pnlCPU;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ pong/pong/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        int ballX = 6;
+        int ballY = 6;
+
+        int playerSpeed = 8;
+        int cpuSpeed = 6;
+
+        bool moveUp = false;
+        bool moveDown = false;
+
+        float speedMultiplier = 1.0f;     
+        float acceleration = 0.001f;     
+        float maxSpeedMultiplier = 3.0f;  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlPlayer.Left = 10;
+            pnlPlayer.Top = ClientSize.Height / 2 - pnlPlayer.Height / 2;
+
+            pnlCPU.Left = ClientSize.Width - pnlCPU.Width - 10;
+            pnlCPU.Top = pnlPlayer.Top;
+
+            picBall.Left = ClientSize.Width / 2 - picBall.Width / 2;
+            picBall.Top = ClientSize.Height / 2 - picBall.Height / 2;
+
+            gameTimer.Interval = 20; 
+            gameTimer.Start();
+        }
+
+        private void gameTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (moveUp && pnlPlayer.Top > 0)
+                pnlPlayer.Top -= playerSpeed;
+
+            if (moveDown && pnlPlayer.Bottom < ClientSize.Height)
+                pnlPlayer.Top += playerSpeed;
+
+            picBall.Left += (int)(ballX * speedMultiplier);
+            picBall.Top += (int)(ballY * speedMultiplier);
+
+            if (picBall.Top <= 0 || picBall.Bottom >= ClientSize.Height)
+                ballY = -ballY;
+
+            if (picBall.Bounds.IntersectsWith(pnlPlayer.Bounds))
+                ballX = Math.Abs(ballX);
+
+            if (picBall.Bounds.IntersectsWith(pnlCPU.Bounds))
+                ballX = -Math.Abs(ballX);
+
+            if (pnlCPU.Top + pnlCPU.Height / 2 < picBall.Top)
+                pnlCPU.Top += cpuSpeed;
+            else
+                pnlCPU.Top -= cpuSpeed;
+
+            if (picBall.Left <= 0)
+                GameOver("CPU Wins!");
+
+            if (picBall.Right >= ClientSize.Width)
+                GameOver("You Win!");
+
+            speedMultiplier += acceleration;
+
+            if (speedMultiplier > maxSpeedMultiplier)
+                speedMultiplier = maxSpeedMultiplier;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                moveUp = true;
+
+            if (e.KeyCode == Keys.Down)
+                moveDown = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                moveUp = false;
+
+            if (e.KeyCode == Keys.Down)
+                moveDown = false;
+        }
+
+        void ResetBall()
+        {
+            picBall.Left = ClientSize.Width / 2 - picBall.Width / 2;
+            picBall.Top = ClientSize.Height / 2 - picBall.Height / 2;
+            ballX = -ballX;
+            speedMultiplier = 1.0f;
+        }
+
+        void GameOver(string message)
+        {
+            gameTimer.Stop();
+            MessageBox.Show(message, "Game Over");
+            this.Close();
+        }
+    }
+}
